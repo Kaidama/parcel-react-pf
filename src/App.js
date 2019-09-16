1fd03168703cd51e68ed16ab4ab28362df83adf5
Jimmy Lai <jimmy.lai@codeimmersives.com>
2019-09-16 15:09:52 -0400

    iniitialized with environment setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Link } from "@reach/router"
+const App = () => {
+    return (
+        <>
+        <div>
+        
+        </div>
+        </>
+    )
+}
+
+render(<App />, document.getElementById("root"));
